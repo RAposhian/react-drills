05df31ab0c42023189dc158dedf1d4594e47f26a
Raposhian <ryan.aposhian@gmail.com>
2020-02-25 16:43:08 -0700

    finished the second part

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      foods: ["food1", 'food2', 'food3', 'food4']
+    }
+  }
+  render() {
+  let foodsToDisplay = this.state.foods.map((e, i) => <h2 key = {i} >{e}</h2>)
+    return(
+    <div className="food">{foodsToDisplay}</div>
+    )
+  }
 }
 
 export default App;
